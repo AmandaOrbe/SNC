2cf80e2241a94ef2abad76175728872f1f97d50c
(Amanda Orbe) <garayomiriam@gmail.com>
2019-01-16 15:26:52 +0100

    cards toggle working

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,6 +2,28 @@
 //= require bootstrap
 //= require_tree .
 
-document.getElementById('epilepsia')
+// const epilepsia = document.getElementById('epilepsia')
+
+// Epilepsia.addEventListener("click", (event) => {
+//   console.log(event);
+//   console.log(event.currentTarget.classList.toggle("card__hidden"));
+//    // event.currentTarget.classList.toggle("img-circle");
+// });
 
 
+const cards = document.querySelectorAll('.card')
+
+cards.forEach((card) => {
+  console.log(card);
+  card.addEventListener("click", (event) => {
+    console.log(event);
+    console.log(event.currentTarget.classList.toggle("card__hidden"));
+  });
+
+});
+
+// Epilepsia.addEventListener("click", (event) => {
+//   console.log(event);
+//   console.log(event.currentTarget.classList.toggle("card__hidden"));
+//    // event.currentTarget.classList.toggle("img-circle");
+// });
